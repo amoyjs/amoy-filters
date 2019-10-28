ee2b7d9ffced526330a64061666f91eb672a791a
lilieming <469826069@qq.com>
2019-10-28 18:13:03 +0800

    npm 发布修改

--- filters/weather-cloud/types.d.ts
+++ filters/weather-cloud/types.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="pixi.js" />
-declare module "@amoy/filter-cloud" {
+declare module "@amoy/filter-weather-cloud" {
     export class AmoyWeatherCloudFilter extends PIXI.Filter {
         constructor(delta?:number);
         delta:number;
--- filters/white-black-sketch/types.d.ts
+++ filters/white-black-sketch/types.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="pixi.js" />
-declare module "@amoy/filter-lens-halo" {
-    export class AmoyLensHaloFilter extends PIXI.Filter {
+declare module "@amoy/filter-white-black-sketch" {
+    export class AmoyWhiteBlackSketchFilter extends PIXI.Filter {
         constructor(invertColor?:boolean, delta?:number);
         invertColor:boolean;
         delta:number;
